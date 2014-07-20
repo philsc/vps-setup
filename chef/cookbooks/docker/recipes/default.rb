700d30c4ee05495a097b41b197d2d56a81d1020e
Philipp Schrader <philipp.schrader@gmail.com>
2014-07-20 01:05:13 +0000

    chef: Install docker.

---
+++ chef/cookbooks/docker/recipes/default.rb
@@ -0,0 +1,13 @@
+# Add the repository from docker.com for the latest images.
+apt_repository "lxc-docker" do
+  uri "https://get.docker.io/ubuntu"
+  distribution "docker"
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+end
+
+# Install the latest docker package.
+package "lxc-docker" do
+  action :install
+end
